54106947935bdeac4d92d104766c16aa18786d6d
pooladpoor <pwladpwr@gmail.com>
2024-09-22 15:19:49 +0330

    زاشchange product/add_to_cart/product_id -> product/add_to_cart/ : Adding all products at once instead of adding one by one

--- product/views.py
+++ product/views.py
@@ -14,17 +14,47 @@ from django.shortcuts import get_object_or_404
 from .serializers import ProductSerializer
 from .serializer import ComentSerializer
 
+# class AddToCartView(LoginRequiredMixin, APIView):
+#     @extend_schema(
+#         summary="افزودن محضول به سبد خرید",
+#     )
+#     def get(self, request, product_id):
+#         product = get_object_or_404(Product, id=product_id)
+#         cart, created = Cart.objects.get_or_create(user=request.user)
+#         cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+#         cart_item.quantity += 1  # تعداد محصول را افزایش می‌دهد
+#         cart_item.save()
+#         return Response(None, status.HTTP_201_CREATED)
 
-class AddToCartView(LoginRequiredMixin, APIView):
-    @extend_schema(
-        summary="افزودن محضول به سبد خرید",
-    )
-    def get(self, request, product_id):
+
+class ListAddToCartView(LoginRequiredMixin, APIView):
+    def setup(self, request, *args, **kwargs):
+        self.cart , created = Cart.objects.get_or_create(user=request.user)
+        super().setup(request, *args, **kwargs)
+        
+    def add_to_cart(self, request, product_id, number):
         product = get_object_or_404(Product, id=product_id)
-        cart, created = Cart.objects.get_or_create(user=request.user)
-        cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
-        cart_item.quantity += 1  # تعداد محصول را افزایش می‌دهد
+        cart_item, created = CartItem.objects.get_or_create(cart=self.cart, product=product)
+        cart_item.quantity += number  # تعداد محصول را افزایش می‌دهد
         cart_item.save()
+    
+    @extend_schema(
+        summary="افزودن محصولات سبد خرید",
+        description="آیدی هر محصول و تعدادش رو به فرمت زیر بفرستید",
+        request={
+            'application/json': {
+                'example': {
+                    'product_id 1': 'number 1',
+                    'product_id 2': 'number 2'
+                            }}},
+                    )
+    def post(self, request: Request):
+        data: dict = request.data
+        try:
+            for product_id, number in data.items():
+                self.add_to_cart(request, product_id, number)
+        except:
+            return Response(None, status.HTTP_400_BAD_REQUEST)
         return Response(None, status.HTTP_201_CREATED)
 
 
